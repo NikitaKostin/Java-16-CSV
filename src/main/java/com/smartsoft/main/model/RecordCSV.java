795189a31ed1c3b9068e2e44207a081b1aa447a1
Nikita Kostin <n.kostin11@mail.ru>
2021-06-04 12:52:23 +0300

    Add description for split CSV

--- src/main/java/com/smartsoft/main/model/RecordCSV.java
+++ src/main/java/com/smartsoft/main/model/RecordCSV.java
@@ -20,6 +20,19 @@ public record RecordCSV(String ssoid,
                         Date ymdh) {
 
     public static RecordCSV getNewRecord(String recordData) {
+        /*
+        ;           // Split on semicolon
+        (?=         // Followed by
+           (?:      // Start a non-capture group
+             [^"]*  // 0 or more non-quote characters
+             "      // 1 quote
+             [^"]*  // 0 or more non-quote characters
+             "      // 1 quote
+           )*       // 0 or more repetition of non-capture group (multiple of 2 quotes will be even)
+           [^"]*    // Finally 0 or more non-quotes
+           $        // Till the end (This is necessary, else every comma will satisfy the condition)
+        )
+         */
         String[] splittedData = recordData.split(";(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
         return new RecordCSV(
                 splittedData[0],
