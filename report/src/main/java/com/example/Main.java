6633b42a69974af458c34c02d3fa3ac07002c741
Nikita Kostin <n.kostin11@mail.ru>
2021-06-24 14:55:40 +0300

    Add report

---
+++ report/src/main/java/com/example/Main.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import static com.example.report.Report.getReport;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(getReport("\u0020Наша любимая Java!)"));
+    }
+}
---
+++ report/src/main/java/com/example/report/Report.java
@@ -0,0 +1,63 @@
+package com.example.report;
+
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+
+public class Report {
+    public static String getReport(String text) {
+        var punctuations = 0L;
+        var whiteSpaces = 0L;
+        var words = new ArrayList<String>();
+        var wordsFreq = new LinkedHashMap<String, Long>();
+
+        var patternWord = Pattern.compile("[A-Za-zА-Яа-яёЁ]+");
+        var matcherWord = patternWord.matcher(text);
+
+        var patternPunctuations = Pattern.compile("[^A-Za-zА-Яа-яёЁ\\s]");
+        var matcherPunctuations = patternPunctuations.matcher(text);
+
+        var patternWhiteSpaces = Pattern.compile("\\s|\\n|\\r|\\t");
+        var matcherWhiteSpaces = patternWhiteSpaces.matcher(text);
+
+
+        while (matcherWord.find()) {
+            words.add(matcherWord.group());
+        }
+        while (matcherPunctuations.find()) {
+            punctuations++;
+        }
+        while (matcherWhiteSpaces.find()) {
+            whiteSpaces++;
+        }
+
+        var maxLengthString = words.stream().mapToInt(String::length).max().orElse(-1);
+        var maxLengthCurrent = 0L; // counter for remove less length string
+        for (int i = 0; i < maxLengthString; i++) {
+            for (var word : words) {
+                if (word.length() > maxLengthCurrent) {
+                    for (int j = 0; j + i < word.length(); j++) {
+                        var subString = word.substring(j, j + i + 1).toLowerCase();
+                        wordsFreq.computeIfPresent(subString, (key, count) -> ++count);
+                        wordsFreq.putIfAbsent(subString, 1L);
+                    }
+                }
+            }
+            maxLengthCurrent++;
+        }
+        var wordFreqString = wordsFreq.keySet().stream()
+                .map(key -> key + " - " + wordsFreq.get(key))
+                .collect(Collectors.joining("\n"));
+
+        return """
+                    Анализ текста
+                Всего в тексте слов – %s,
+                Знаков препинания и прочих символов – %s,
+                Пробелов и прочих отступов – %s.
+                Встречаемость сочетаний букв:
+                %s              
+                """.formatted(words.size(), punctuations, whiteSpaces, wordFreqString);
+
+    }
+}
